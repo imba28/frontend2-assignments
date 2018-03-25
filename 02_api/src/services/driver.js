7435f22146a20d76efe5338499fd9e621e6ed87b
Lukas Gruber <lgruber.mmt-b2016@fh-salzburg.ac.at>
2018-03-26 01:55:19 +0200

    feat(service): add driver cache array

--- 02_api/src/services/driver.js
+++ 02_api/src/services/driver.js
@@ -1,15 +1,30 @@
+import driver from "../template/driver";
+
+let driverCache = [];
+
 export default function (slug = null) {
     const url = slug ?
         `http://ergast.com/api/f1/drivers/${slug}.json` :
         'http://ergast.com/api/f1/drivers.json';
-    
+
     return new Promise((resolve, reject) => {
-        fetch(url)
-            .then(response => response.json())
-            .then(json => json.MRData.DriverTable.Drivers)
-            .then((drivers) => {
-                resolve(drivers);
-            })
-            .catch(err => reject(err));
-    })
+        if (driverCache.length > 0) {
+            if (slug) {
+                resolve(driverCache.find((item) => item.driverId === slug));
+            } else {
+                resolve(driverCache);
+            }
+        } else {
+            fetch(url)
+                .then(response => response.json())
+                .then(json => json.MRData.DriverTable.Drivers)
+                .then((drivers) => {
+                    if (slug) [drivers] = drivers;
+                    else driverCache = drivers;
+
+                    resolve(drivers);
+                })
+                .catch(err => reject(err));
+        }
+    });
 }
--- 02_api/src/template/driver.js
+++ 02_api/src/template/driver.js
@@ -3,7 +3,6 @@ import driver from '@/services/driver';
 
 export default function (slug) {
     driver(slug)
-        .then(json => json[0])
         .then(driver => {
             console.log(driver);
             const container = document.getElementById('container');
