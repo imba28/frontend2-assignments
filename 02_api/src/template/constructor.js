f232503b94e151b4fcbbbd62601c4c05c27a121f
Lukas Gruber <lgruber.mmt-b2016@fh-salzburg.ac.at>
2018-03-28 15:52:28 +0200

    feat(page): add constructor page

---
+++ 02_api/src/template/constructor.js
@@ -0,0 +1,23 @@
+import ConstructorPage from '@/template/constructor.hbs';
+import ErrorPage from '@/template/error.hbs';
+import driver from '@/services/driver';
+import constructor from '@/services/constructor';
+
+export default function (slug) {
+    Promise.all([driver({manufacturer: slug}), constructor({id: slug})])
+        .then(json => {
+            let [drivers, constructor] = json;
+            const container = document.getElementById('container');
+
+            if (constructor.total == 1) {
+                container.innerHTML = ConstructorPage({
+                    drivers: drivers.data,
+                    constructor: constructor.data[0]
+                });
+            } else {
+                container.innerHTML = ErrorPage({
+                    error: 'Konstrukteur wurde nicht gefunden!'
+                });
+            }
+        });
+}
