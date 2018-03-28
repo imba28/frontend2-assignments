fa9faf2a384dabe38b821dedf9a575c5458345cc
Lukas Gruber <lgruber.mmt-b2016@fh-salzburg.ac.at>
2018-03-28 15:51:47 +0200

    fix(routes): fix route names singular/plural

--- 02_api/src/js/routes.js
+++ 02_api/src/js/routes.js
@@ -1,6 +1,7 @@
 import DriversPage from '@/template/drivers';
 import DriverPage from '@/template/driver';
 import ErrorPage from '@/template/error.hbs';
+import ConstructorPage from '@/template/constructor';
 import ConstructorsPage from '@/template/constructors';
 
 const container = document.getElementById('container');
@@ -12,8 +13,9 @@ function render(template, ctx) {
 export default {
     '/drivers': () => DriversPage(),
     '/drivers/page/:page': (page) => DriversPage(page),
-    '/driver/:slug': (driverID) => DriverPage(driverID),
+    '/drivers/:slug': (driverID) => DriverPage(driverID),
     '/constructors': () => ConstructorsPage(),
     '/constructors/page/:page': (page) => ConstructorsPage(page),
+    '/constructors/:constructor': (constructor) => ConstructorPage(constructor),
     '*': () => render(ErrorPage, {error: '404 - Page not found'})
 }
