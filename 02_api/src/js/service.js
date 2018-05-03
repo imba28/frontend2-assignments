f6281386ff3054e25f856d9453431f4d02e846b6
Lukas Gruber <lgruber.mmt-b2016@fh-salzburg.ac.at>
2018-05-03 12:03:25 +0200

    feat(api): add include relationships in json api query
    e.g new Service('Drivers').get({ id: 'somename', include: 'Constructor' }) fetches
    /drivers/somename.json and /drivers/somename/constructors.json

--- 02_api/src/js/service.js
+++ 02_api/src/js/service.js
@@ -1,21 +1,65 @@
 import Result from '@/js/result';
 
+const baseUrl = "http://ergast.com/api/f1";
+
 class Service {
     constructor(name) {
         this.name = name;
     }
 
-    get(url) {
-        return new Promise((resolve, reject) => {
+    get(opt = {}) {
+        let {
+            offset = 0,
+                id = null,
+                limit = 30,
+                include = [],
+                map = {}
+        } = opt;
+
+        if (!Array.isArray(include)) {
+            include = [include]
+        }
+
+        const url = `${baseUrl}/${this.name.toLowerCase()}s${id ? `/${id.toLowerCase()}` : ``}.json?offset=${offset}&limit=${limit}`;
+        console.log(url);
+
+        const p = new Promise((resolve, reject) => {
             fetch(url)
                 .then(response => response.json())
-                .then(json => new Result(json.MRData[`${this.name}Table`][`${this.name}s`], json.MRData.offset, json.MRData.total))
-                .then((response) => {
-                    resolve(response);
-                    return response;
+                .then(json => {
+                    // map object key names of json response. 
+                    const table = map[this.name] ? map[this.name] : this.name;
+
+                    return new Result(
+                        json.MRData[`${table}Table`][`${table}s`],
+                        json.MRData.offset,
+                        json.MRData.total
+                    );
                 })
-                .catch(err => reject(err));
+                .then(result => resolve(result))
+                .catch(err => reject(new Error(err)));
         });
+
+        if (include.length > 0) {
+            const a = include.map(resource => {
+                const options = {
+                    id: `${id}/${resource}s`,
+                    map: {}
+                }   
+                // if mapping name for resource is defined map root table name (this.name) to resource
+                options.map[this.name] = map[resource] ? map[resource] : resource;
+
+                return this.get(options);
+            });
+
+            a.unshift(p)
+
+            return new Promise((resolve) => {
+                Promise.all(a).then(data => resolve(data));
+            });
+        }
+
+        return p;
     }
 }
 
